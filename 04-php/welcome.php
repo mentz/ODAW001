8a91907c11e4be8a0553c8a6c9a9d536750d6cf9
The Mother <lucaslittermentz@gmail.com>
2019-09-19 16:51:35 -0300

    Tem que mandar o que tem ne :/

---
+++ 04-php/welcome.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="my_style.css" />
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <h1>Exercícios 19/09</h1>
+    <hr width="50%" />
+    <?php
+
+    function valida_email($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    function valida_senha($senha)
+    {
+        return preg_match("/^(?=[a-z])(?=[A-Z])(?=[0-9])(?=[!-\/:-?])(?=.{6,})/", $senha);
+    }
+
+    function validate_post($args)
+    {
+        if (
+            array_key_exists("name", $args) &&
+            array_key_exists("email", $args) &&
+            array_key_exists("senha", $args) &&
+            array_key_exists("gender", $args) &&
+            array_key_exists("bio", $args) &&
+            array_key_exists("termos", $args)
+        ) {
+            return valida_email($args["email"]) && valida_senha($args["senha"]);
+        }
+        return false;
+    }
+
+    if (validate_post($_POST)) {
+        print('<div id="success"><h2>Parabéns!</h2><h1>Está cadastrado!</h1>Seus dados são: <br />');
+        print('<table><tr><th>Campo</th><th>valor</th></tr>');
+        $campos = ["name", "email", "gender", "bio"];
+        $opcionais = ["aluno", "professor"];
+
+        foreach ($campos as $campo) {
+            printf("<tr><td>%s</td><td>%s</td></tr>", $campo, filter_var($_POST[$campo], FILTER_SANITIZE_STRING));
+        }
+        foreach ($opcionais as $campo) {
+            if (array_key_exists($campo, $_POST)) {
+                printf("<tr><td>%s</td><td>True</td></tr>", $campo);
+            }
+        }
+    } else {
+        print('<div id="failure"><h2>Falhou!</h2><h3><a href="cadastro.html">Tente novamente, dessa vez preenchendo todos os campos.</a></h3>');
+        if (array_key_exists("email", $_POST) && !filter_var($args["email"], FILTER_VALIDATE_EMAIL)) {
+            print('<h1>e lembra que o e-mail tem que ser válido</h1>');
+        }
+    }
+    print('</div>');
+    ?>
+</body>
+
+</html>

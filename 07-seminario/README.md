d4805022693e491bbe341b7fae7e8fc160a30558
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2019-09-30 00:13:56 -0300

    Iniciado Lua

---
+++ 07-seminario/README.md
@@ -0,0 +1,9 @@
+# Lua
+
+Usando Lapis 1.7.0
+
+Para ligar o servidor Lapis (OpenResty (NGINX)) use:
+
+'''bash
+$ lapis server
+'''
---
+++ 07-seminario/hello/app.lua
@@ -0,0 +1,11 @@
+local lapis = require("lapis")
+local app = lapis.Application()
+
+app:enable("etlua")
+app.layout = require("views.layout")
+
+app:get("/", function()
+  return { render = "index" }
+end)
+
+return app

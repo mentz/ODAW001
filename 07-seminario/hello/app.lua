06c0b93d0233705332802d9b4d19dec4e3384c87
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2019-10-01 23:29:02 -0300

    Tem coisa ai

--- 07-seminario/hello/app.lua
+++ 07-seminario/hello/app.lua
@@ -4,8 +4,18 @@ local app = lapis.Application()
 app:enable("etlua")
 app.layout = require("views.layout")
 
-app:get("/", function()
+app:get("/", function(self)
+  self.page_title = "O Título é Lava"
+  self.coisas_que_gosto = {
+    "Bananas",
+    "Carros",
+    "Dormir"
+  }
   return { render = "index" }
+end)
+
+app:get("/args", function(self)
+  return { render = "args" }
 end)
 
 return app

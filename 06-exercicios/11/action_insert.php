0b1af326881ae5b9647104603718c01164edfe6c
Arthur Schuelter <arthur.schuelter@gmail.com>
2019-10-17 16:43:06 -0300

    mais codego

--- 06-exercicios/11/action_insert.php
+++ 06-exercicios/11/action_insert.php
@@ -2,29 +2,39 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <link rel="stylesheet" type="text/css" href="my_style.css" />
   <title>Tarefa 11</title>
 </head>
 
 <body>
-    <?php
-    $db = new mysqli('localhost', 'odawphp', 'z8mT6^FqH3Cu*4baq*kz', 'odawphp');
+  <?php
+  include("template_header.php");
+ 
+  $chaves = "(";
+  $valores = "(";
+  foreach($_POST as $chave_davez => $valor_davez){
+    if($key != "send"){
+      $chaves = $chaves . $chave_davez . ",";
+      $valores = $valores . $valor_davez . ",";
+    }
+  }
+  $chaves = substr($chaves, 0, -1);
+  $chaves = $chaves . ");";
 
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $cpf = $_POST["cpf"];
-    $telefone = $_POST["telefone"];
-    $aluno = $_POST["aluno"];
-    $professor = $_POST["professor"];
+  $valores = substr($valores, 0, -1);
+  $valores = $valores . ");";
 
-    $query = <<<EOT
-    INSERT INTO alunos (nome, email, cpf, telefone, aluno, professor) VALUES
-        ($nome, $email, $cpf, $telefone, $aluno, $professor);
-    EOT;
-    ?>
+  $query_insert = "INSERT INTO alunos "  . $chaves . " VALUES " . $valores ;
+  $status = mysql_query($query_insert);
+  if($status){
+    echo '<h1> Inserido com sucesso </h1>'
+  } else {
+    echo '<h1> Não foi inserido problema no banco de dados </h1>'
+  }
+?>
 
 </body>
 

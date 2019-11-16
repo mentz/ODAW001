e700ce4d7451e8b1e2d76df929ce20d25dd6c573
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2019-11-16 20:54:19 -0300

    Qq

--- 07-seminario/Comparacao/fib.lua
+++ 07-seminario/Comparacao/fib.lua
@@ -1,9 +1,23 @@
+-- Função Fibonacci recursiva (lenta)
 function fib(i)
 	if i <= 2 then
-		return 1
+		return 1;
 	else
-		return fib(i-1) + fib(i-2)
-	end
+		return fib(i-1) + fib(i-2);
+	end;
+end;
+
+print("Fib(39) = " .. fib(39))
+
+-- Iterar sobre string
+local str = "String comum 123"
+
+-- forma 1, usando contador no índice
+for i = 1, #str do
+	print(str:sub(i, i))
 end
 
-print(fib(39))
+-- forma 2, usando regex
+for ch in str:gmatch(".") do
+	print(ch)
+end

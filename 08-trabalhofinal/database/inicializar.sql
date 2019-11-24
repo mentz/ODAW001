92edd05693304eb85e04b6bebc4c034fdff03701
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2019-11-23 23:22:19 -0300

    Iniciado DB

--- 08-trabalhofinal/database/inicializar.sql
+++ 08-trabalhofinal/database/inicializar.sql
@@ -7,3 +7,15 @@ CREATE TABLE usuario (
 
 INSERT INTO usuario (nome, email, senha) VALUES
   ('Lucas Litter Mentz', 'lucas', 'mentz');
+
+CREATE TABLE lista (
+  id BIGSERIAL PRIMARY KEY,
+  autor BIGSERIAL NOT NULL REFERENCES usuario (id)
+);
+
+CREATE TABLE tarefa (
+  id BIGSERIAL PRIMARY KEY,
+  estado BOOLEAN NOT NULL,
+  descricao VARCHAR(50) NOT NULL,
+  lista BIGINT NOT NULL REFERENCES lista(id)
+);
